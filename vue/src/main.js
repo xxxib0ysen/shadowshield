93a00bdfb9746b66e8d63e733de81094684e9f3b
xxxib0ysen <xxxib0ysen@gmail.com>
2025-02-15 19:56:52 +0800

    vue集成element-plus

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus) // 注册 Element Plus
+app.mount('#app')
